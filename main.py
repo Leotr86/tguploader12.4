df99535d546a08aca38ac3d8fe769349c935a50c
Cornelio3319 <109839690+Cornelio3319@users.noreply.github.com>
2022-08-17 20:14:50 -0400

    Update main.py

--- main.py
+++ main.py
@@ -228,7 +228,7 @@ def onmessage(update,bot:ObigramClient):
         tl_admin_user = os.environ.get('tl_admin_user')
 
         #set in debug
-        #tl_admin_user = 'obidevel'
+        #tl_admin_user = 'Elpatron022'
 
         jdb = JsonDatabase('database')
         jdb.check_create()
@@ -703,7 +703,7 @@ def onmessage(update,bot:ObigramClient):
                 # set in debug
             #    api_id = 7386053
             #    api_hash = '78d1c032f3aa546ff5176d9ff0e7f341'
-            #    bot_token = '5124841893:AAH30p6ljtIzi2oPlaZwBmCfWQ1KelC6KUg'
+            #    bot_token = '5723785621:AAFURbokdUPbEgE6RDATI0YsAHtwCzlObUY'
 
             #    chat_id = int(update.message.chat.id)
             #    message_id = int(update.message.message_id)
@@ -804,7 +804,7 @@ def main():
     bot_token = os.environ.get('bot_token')
     print('init bot.')
     #set in debug
-    #bot_token = '5350913309:AAHdMDFYF8Aeh-lkU1uHfKAPvSnXVg3oUYI'
+    #bot_token = '5723785621:AAFURbokdUPbEgE6RDATI0YsAHtwCzlObUY'
     bot = ObigramClient(bot_token)
     bot.onMessage(onmessage)
     bot.onCallbackData('/cancel ',cancel_task)
